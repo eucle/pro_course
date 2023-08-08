72ca4e95d812518ca07b8950820327dba2fb33b7
Rus Esakov <eucle@ya.ru>
2023-08-08 23:08:37 +0300

    some new solutions added

---
+++ 4.2/task_4.2_21.py
@@ -0,0 +1,14 @@
+import csv
+
+
+with open('student_counts.csv', 'r', encoding='utf-8') as file:
+    rows = csv.DictReader(file, delimiter=',')
+    columns = ['year'] +\
+        sorted(rows.fieldnames[1:], key=lambda x: (int(x.split('-')[0]), x.split('-')[-1]))
+    new_rows: list = list(rows)
+
+with open('sorted_student_counts.csv', 'w', encoding='utf-8') as file:
+    writer = csv.DictWriter(file, fieldnames=columns, delimiter=',')
+    writer.writeheader()
+    for row in new_rows:
+        writer.writerow(row)
